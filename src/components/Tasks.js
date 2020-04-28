6ab5cc68b4ddbf1d40a9a3c4dfdcd13ae4c78646
Nikolay <30789678+Nikolaiy@users.noreply.github.com>
2020-04-28 20:28:21 +0500

    added component AddTask

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,6 +4,7 @@ import { useTasks } from '../hooks';
 import { collatedTasks } from '../constants';
 import { useSelectedProjectValue, useProjectsValue } from '../context';
 import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { AddTask } from './AddTask';
 
 
 export const Tasks = () => {
@@ -40,6 +41,7 @@ export const Tasks = () => {
                     </li>
     ))}
             </ul>
+            <AddTask />
         </div>
     )
 }
